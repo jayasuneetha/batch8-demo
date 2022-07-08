5558582e5b172e797863d7780e8eab1f6744a632
email <suee535@gmail.com>
2022-07-08 13:30:25 +0530

    created one new file Addition.java

--- addition.java
+++ addition.java
@@ -1,13 +1,11 @@
-
-class addition
+class Addition
 {
 public static void main(string args[])
 {
 int num1,num2,res;
-num=20;
-num=45;
-res=num1+num2'
+num1=20;
+num2=45;
+res=num1+num2;
 system.out.println("The result is" +res);	
 }
-
 }
