43fc4a329753edff42dd8ccc1afcdbf906a5fe39
email <suee535@gmail.com>
2022-07-09 08:42:36 +0530

    Modified num1 & num2 values

--- addition.java
+++ addition.java
@@ -3,8 +3,8 @@ class Addition
 public static void main(string args[])
 {
 int num1,num2,res;
-num1=20;
-num2=45;
+num1=20050;
+num2=45050;
 res=num1+num2;
 system.out.println("The sum" +res);	
 //This is a comment
