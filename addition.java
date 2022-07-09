407c227675f72069b5ecb887a447681d5f8cdeb5
email <suee535@gmail.com>
2022-07-09 08:57:58 +0530

    changed num1 & num2 values in sunitha branch

--- addition.java
+++ addition.java
@@ -3,8 +3,8 @@ class Addition
 public static void main(string args[])
 {
 int num1,num2,res;
-num1=20;
-num2=45;
+num1=20057;
+num2=45058;
 res=num1+num2;
 system.out.println("The sum" +res);	
 //This is a comment
