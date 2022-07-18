24c0d52dc4340d79480b8854126233d159f8d9c5
email <suee535@gmail.com>
2022-07-18 23:18:56 +0530

    Added some code to addition.java

--- addition.java
+++ addition.java
@@ -6,6 +6,11 @@ int num1,num2,res;
 num1=20057;
 num2=45058;
 res=num1*num2;
-system.out.println("The sum" +res);	
+system.out.println("The sum" +res);
+if(res>0)
+	system.out.println("This is a possitive number");
+else
+	system.out.println("This is a negative number");
+//This is a comment
 }
 }
